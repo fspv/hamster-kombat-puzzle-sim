a88517112c7e89116d9c158504d52e99248f647f
Pavel Safronov <pv.safronov@gmail.com>
2024-07-29 21:08:39 +0100

    new minigame

--- rush-hour.js
+++ rush-hour.js
@@ -542,6 +542,7 @@ function startCountdown(duration) {
 }
 
 let puzzles = {
+  "2024-07-30": "BCooLLBCDDDKAAEooKooEFFKooEGHHIIIGoo",
   "2024-07-29": "BooDDoBCCCEFAALoEFooLGGFKKLIHHJJJIoo",
   "2024-07-28": "BBooEFoCDDEFoCAAEoooLoGGKKLHooJJJHoo",
   "2024-07-27": "oBBBoKooCFJKAACFJoooDFHHooDGGIoEEooI",
